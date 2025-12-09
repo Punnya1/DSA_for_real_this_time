3cc9050556e7aa8e8685e4f047fe83204915c70a
Punnya Khurana <punnyakhurana@gmail.com>
2025-12-09 09:03:01 +0530

    First 5 basic ass problems

---
+++ pattern_problems/triangle_patterns.py
@@ -0,0 +1,24 @@
+# def right_angled(n):
+#     for i in range(1,n+1):
+#         print("*"*i)
+
+# right_angled(5)
+
+# def right_angle_continous_numbers(n):
+#     for i in range(1,n+1):
+#         for j in range(1,i+1):
+#             print(j, end=" ")
+#         print()
+# right_angle_continous_numbers(5)
+
+# def right_angle_with_numbers(n):
+#     for i in range(1,n+1):
+#         print(f"{i} "*i)
+
+# right_angle_with_numbers(5)
+
+# def star_rectangle(n):
+#     for i in range(n):
+#         print("*"*n)
+
+# star_rectangle(5)
